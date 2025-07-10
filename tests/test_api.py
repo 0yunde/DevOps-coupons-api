10c0457cc99179a421154e0788f261ab475bffc8
0yunde <alejandro.yovaniniz@gmail.com>
2025-07-09 22:56:26 -0400

    Add covarage artifactn report

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,6 @@
 from app.api import app
 
-def test_reserva_exitosa():
+def test_api_coupons():
     client = app.test_client()
     res = client.post('/price', json={"price": 990, "coupon": "SALES10"})
     assert res.status_code == 200
--- tests/test_coupons.py
+++ tests/test_coupons.py
@@ -1,13 +1,13 @@
 from app.coupons import aplicate_coupon, calculate_price_final
 
 def test_discount_discount_offer10():
-    assert aplicate_coupon(100, "SALES10") == 90.0
+    assert aplicate_coupon(100, "SALES10") == 90
 
 def test_discount_discount_super20():
-    assert aplicate_coupon(200, "SUPER20") == 160.0
+    assert aplicate_coupon(200, "SUPER20") == 160
 
 def test_discount_discount_welcome():
-    assert aplicate_coupon(100, "WELCOME") == 85.0
+    assert aplicate_coupon(100, "WELCOME") == 85
 
 def test_discount_discount_fake():
     assert aplicate_coupon(990, "FAKE") == 990   
